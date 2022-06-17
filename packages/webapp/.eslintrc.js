c8648c924cebb004b87337a5f444124c1fa075b8
Luoshan Zheng <ge47kod@mytum.de>
2022-06-17 17:43:21 +0200

    lrz/rtk: Add toast tab components, remove test configurations

--- packages/webapp/.eslintrc.js
+++ packages/webapp/.eslintrc.js
@@ -6,22 +6,6 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["demo/**/*.ts", "demo/**/*.tsx"],
-      extends: "../../eslintrc.ts.react.base.json",
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.demo.json"],
-      },
-    },
-    {
-      files: ["test/**/*.ts", "test/**/*.tsx"],
-      extends: "../../eslintrc.ts.react.jest.base.json",
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./test/tsconfig.json"],
-      },
-    },
-    {
       files: ["webpack.config.ts", "webpack.config.umd.ts"],
       extends: "../../eslintrc.ts.base.json",
       parserOptions: {
