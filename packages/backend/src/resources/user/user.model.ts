87ddd639642391138e89eaa53ce989bb4cbcea31
simon-roe <s2401r@gmail.com>
2022-06-18 14:31:11 +0200

    minor changes

--- packages/backend/src/resources/user/user.model.ts
+++ packages/backend/src/resources/user/user.model.ts
@@ -1,46 +1,46 @@
-import {model, Schema, Types} from "mongoose";
+import { model, Schema, Types } from "mongoose";
 import bcrypt from "bcrypt";
 import User from "./user.interface";
 
 const UserSchema = new Schema<User>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        virtualAccountId: {
-            type: Types.ObjectId,
-            ref: "VirtualAccount",
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    virtualAccountId: {
+      type: Types.ObjectId,
+      ref: "VirtualAccount",
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 UserSchema.pre<User>("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-    this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 
-    next();
+  next();
 });
 
 UserSchema.methods.isValidPassword = async function (
-    password: string
+  password: string
 ): Promise<boolean | Error> {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 export default model<User>("User", UserSchema);
