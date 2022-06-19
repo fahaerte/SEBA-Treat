07c9c8f1d334e2a086486740ae5fa3ed513b3bce
johannes <johannes.wiest@muenchen-mail.de>
2022-06-19 12:30:43 +0200

    update fix in mongo db

--- packages/backend/src/resources/mealOffer/mealOffer.model.ts
+++ packages/backend/src/resources/mealOffer/mealOffer.model.ts
@@ -1,62 +1,62 @@
-import {model, Schema, Types} from "mongoose";
+import { model, Schema, Types } from "mongoose";
 import MealOffer from "../mealOffer/mealOffer.interface";
 
 const MealOfferSchema = new Schema<MealOffer>(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        user: {
-            type: Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        categories: [
-            {
-                type: Types.ObjectId,
-                required: true,
-            },
-        ],
-        allergens: [
-            {
-                type: Types.ObjectId,
-                required: true,
-            },
-        ],
-        startDate: {
-            type: Date,
-            required: true,
-        },
-        endDate: {
-            type: Date,
-            required: true,
-        },
-        portions: {
-            type: Number,
-            required: true,
-            min: 1,
-        },
-        pickUpDetails: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-            min: 0,
-        },
-        transactionFee: {
-            type: Number,
-            default: 0,
-            min: 0,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    categories: [
+      {
+        type: Types.ObjectId,
+        required: true,
+      },
+    ],
+    allergens: [
+      {
+        type: Types.ObjectId,
+        required: true,
+      },
+    ],
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    portions: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    pickUpDetails: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    transactionFee: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  { timestamps: true }
 );
 
 export default model<MealOffer>("MealOffer", MealOfferSchema);
--- packages/backend/src/resources/mealReservation/mealReservation.service.ts
+++ packages/backend/src/resources/mealReservation/mealReservation.service.ts
@@ -1,4 +1,4 @@
-import {Service} from "typedi";
+import { Service } from "typedi";
 import MealReservationSchema from "./mealReservation.model";
 import MealReservation from "./mealReservation.interface";
 import User from "../user/user.interface";
@@ -10,114 +10,113 @@ import HttpException from "../../utils/exceptions/http.exception";
 
 @Service()
 class MealReservationService {
-    private mealReservation = MealReservationSchema;
+  private mealReservation = MealReservationSchema;
 
-    constructor(private readonly MealOfferService: MealOfferService) {
-    }
+  constructor(private readonly MealOfferService: MealOfferService) {}
 
-    public async create(
-        mealOfferId: string,
-        buyer: User
-    ): Promise<MealReservation | Error> {
-        const mealOffer = (await this.MealOfferService.getMealOffer(
-            mealOfferId
-        )) as MealOffer;
-        const newMealReservation = {
-            mealOffer: mealOffer._id,
-            buyer: buyer._id,
-            seller: mealOffer.user,
-            reservationState: MealReservationState.PENDING,
-        } as MealReservation;
-        return await this.mealReservation.create(newMealReservation);
-    }
+  public async create(
+    mealOfferId: string,
+    buyer: User
+  ): Promise<MealReservation | Error> {
+    const mealOffer = (await this.MealOfferService.getMealOffer(
+      mealOfferId
+    )) as MealOffer;
+    const newMealReservation = {
+      mealOffer: mealOffer._id,
+      buyer: buyer._id,
+      seller: mealOffer.user,
+      reservationState: MealReservationState.PENDING,
+    } as MealReservation;
+    return await this.mealReservation.create(newMealReservation);
+  }
 
-    public async getMealReservation(
-        mealReservationId: string,
-        user: User
-    ): Promise<MealReservation | Error> {
-        const mealReservation = (await this.mealReservation.findById(
-            mealReservationId
-        )) as MealReservation;
-        if (this.checkIfUserIsAllowedToGetReservation(mealReservation, user)) {
-            return mealReservation;
-        }
-        throw new HttpException(403, "not allowed");
+  public async getMealReservation(
+    mealReservationId: string,
+    user: User
+  ): Promise<MealReservation | Error> {
+    const mealReservation = (await this.mealReservation.findById(
+      mealReservationId
+    )) as MealReservation;
+    if (this.checkIfUserIsAllowedToGetReservation(mealReservation, user)) {
+      return mealReservation;
     }
+    throw new HttpException(403, "not allowed");
+  }
 
-    public async setMealReservationStateToSellerAccepted(
-        mealReservationId: string,
-        user: User
-    ): Promise<MealReservation | Error> {
-        return await this.setMealReservationStatus(
-            mealReservationId,
-            user,
-            MealReservationState.PENDING,
-            MealReservationState.SELLER_ACCEPTED
-        );
-    }
+  public async setMealReservationStateToSellerAccepted(
+    mealReservationId: string,
+    user: User
+  ): Promise<MealReservation | Error> {
+    return await this.setMealReservationStatus(
+      mealReservationId,
+      user,
+      MealReservationState.PENDING,
+      MealReservationState.SELLER_ACCEPTED
+    );
+  }
 
-    public async setMealReservationStateToSellerRejected(
-        mealReservationId: string,
-        user: User
-    ): Promise<MealReservation | Error> {
-        return await this.setMealReservationStatus(
-            mealReservationId,
-            user,
-            MealReservationState.PENDING,
-            MealReservationState.SELLER_REJECTED
-        );
-    }
+  public async setMealReservationStateToSellerRejected(
+    mealReservationId: string,
+    user: User
+  ): Promise<MealReservation | Error> {
+    return await this.setMealReservationStatus(
+      mealReservationId,
+      user,
+      MealReservationState.PENDING,
+      MealReservationState.SELLER_REJECTED
+    );
+  }
 
-    public async setMealReservationStateToBuyerConfirmed(
-        mealReservationId: string,
-        user: User
-    ): Promise<MealReservation | Error> {
-        return await this.setMealReservationStatus(
-            mealReservationId,
-            user,
-            MealReservationState.SELLER_ACCEPTED,
-            MealReservationState.BUYER_CONFIRMED
-        );
-    }
+  public async setMealReservationStateToBuyerConfirmed(
+    mealReservationId: string,
+    user: User
+  ): Promise<MealReservation | Error> {
+    return await this.setMealReservationStatus(
+      mealReservationId,
+      user,
+      MealReservationState.SELLER_ACCEPTED,
+      MealReservationState.BUYER_CONFIRMED
+    );
+  }
 
-    public async setMealReservationStateToBuyerRejected(
-        mealReservationId: string,
-        user: User
-    ): Promise<MealReservation | Error> {
-        return await this.setMealReservationStatus(
-            mealReservationId,
-            user,
-            MealReservationState.SELLER_ACCEPTED,
-            MealReservationState.BUYER_REJECTED
-        );
-    }
+  public async setMealReservationStateToBuyerRejected(
+    mealReservationId: string,
+    user: User
+  ): Promise<MealReservation | Error> {
+    return await this.setMealReservationStatus(
+      mealReservationId,
+      user,
+      MealReservationState.SELLER_ACCEPTED,
+      MealReservationState.BUYER_REJECTED
+    );
+  }
 
-    private async setMealReservationStatus(
-        mealReservationId: string,
-        user: User,
-        expectedState: MealReservationStateEnum,
-        newState: MealReservationStateEnum
-    ): Promise<MealReservation | Error> {
-        const mealReservation = (await this.getMealReservation(
-            mealReservationId,
-            user
-        )) as MealReservation;
-        if (mealReservation.reservationState === expectedState) {
-            mealReservation.reservationState = newState;
-            return await mealReservation.save();
-        }
-        throw new HttpException(400, "Wrong state");
+  private async setMealReservationStatus(
+    mealReservationId: string,
+    user: User,
+    expectedState: MealReservationStateEnum,
+    newState: MealReservationStateEnum
+  ): Promise<MealReservation | Error> {
+    const mealReservation = (await this.getMealReservation(
+      mealReservationId,
+      user
+    )) as MealReservation;
+    if (mealReservation.reservationState === expectedState) {
+      mealReservation.reservationState = newState;
+      return await mealReservation.save();
     }
+    throw new HttpException(400, "Wrong state");
+  }
 
-    private checkIfUserIsAllowedToGetReservation(
-        mealReservation: MealReservation,
-        user: User
-    ): boolean {
-        return (
-            String(user._id) === String(mealReservation.buyer) ||
-            String(user._id) === String(mealReservation.seller)
-        );
-    }
+  private checkIfUserIsAllowedToGetReservation(
+    mealReservation: MealReservation,
+    user: User
+  ): boolean {
+    return (
+      String(user._id) === String(mealReservation.buyer) ||
+      String(user._id) === String(mealReservation.seller)
+    );
+  }
 }
 
 export default MealReservationService;
