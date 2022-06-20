360f7235554b1e33e9898e8bd6714da3ef25ec7d
johannes <johannes.wiest@muenchen-mail.de>
2022-06-20 17:32:22 +0200

    Completing user model and adding address Ref #7

--- packages/backend/src/resources/address/address.interface.ts
+++ packages/backend/src/resources/address/address.interface.ts
@@ -1,9 +1,9 @@
 interface Address {
-    street: string;
-    houseNumber: string;
-    postalCode: string;
-    city: string;
-    country: string
+  street: string;
+  houseNumber: string;
+  postalCode: string;
+  city: string;
+  country: string;
 }
 
 export default Address;
--- packages/backend/src/resources/user/user.validation.ts
+++ packages/backend/src/resources/user/user.validation.ts
@@ -3,22 +3,22 @@ import j2s from "joi-to-swagger";
 import AddressValidation from "../address/address.validation";
 
 const register = Joi.object({
-    email: Joi.string().email().required().example('test@user.de'),
-    firstName: Joi.string().required().example('testuser'),
-    lastName: Joi.string().required().example("Mustermann"),
-    password: Joi.string().min(6).required().example('pa55word'),
-    birthdate: Joi.date().required(),
-    address: AddressValidation.create.required()
+  email: Joi.string().email().required().example("test@user.de"),
+  firstName: Joi.string().required().example("testuser"),
+  lastName: Joi.string().required().example("Mustermann"),
+  password: Joi.string().min(6).required().example("pa55word"),
+  birthdate: Joi.date().required(),
+  address: AddressValidation.create.required(),
 });
 
 const login = Joi.object({
-    email: Joi.string().email().required().example('teat@user.de'),
-    password: Joi.string().required().example('pa55word'),
+  email: Joi.string().email().required().example("teat@user.de"),
+  password: Joi.string().required().example("pa55word"),
 });
 
-export default {register, login};
+export default { register, login };
 
 export const User = {
-    RegisterUser: j2s(register).swagger,
-    LoginUser: j2s(login).swagger,
+  RegisterUser: j2s(register).swagger,
+  LoginUser: j2s(login).swagger,
 };
