bc76b117687beb34b5e1d9d3c5897cebb83179c7
Fabian Härtel <fabian.haertel@tum.de>
2022-06-23 16:41:30 +0200

    all changes #13

--- packages/webapp/src/screens/LoginScreen.tsx
+++ packages/webapp/src/screens/LoginScreen.tsx
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Button, Form, FormHelper } from "@treat/webapp/src/components/";
 import { IFormRow } from "@treat/webapp/src/components/";
-import {IUser} from "@treat/lib-common";
+import { IUser } from "@treat/lib-common";
 
 const LoginScreen = () => {
+  const elements: IFormRow<IUser>[] = [
+    [
+      FormHelper.createInput({
+        formKey: "email",
+        label: "Email",
+        props: {
+          type: "text",
+        },
+        rules: {
+          required: { value: true },
+        },
+        defaultValue: "max@mustermann.de",
+      }),
+      FormHelper.createInput({
+        formKey: "password",
+        label: "Password",
+        props: {
+          type: "text",
+        },
+        defaultValue: "",
+      }),
+    ],
+  ];
 
- const elements: IFormRow<IUser>[] = [
-  [
-   FormHelper.createInput({
-    formKey: "email",
-    label: "Email",
-    props: {
-     type: "text",
-    },
-    rules: {
-     required: { value: true },
-    },
-    defaultValue: "max@mustermann.de",
-   }),
-   FormHelper.createInput({
-    formKey: "password",
-    label: "Password",
-    props: {
-     type: "text",
-    },
-    defaultValue: "",
-   }),
-  ]
- ];
+  const handleSignIn = (data: IUser) => {
+    console.log(data.email + "  " + data.password);
+  };
 
- const handleSignIn = (data: IUser) => {
-  console.log(data.email + "  " + data.password);
- };
-
- return (
-     <>
-      <Form<IUser> elements={elements} onSubmit={handleSignIn}/>
-     </>
- );
+  return (
+    <>
+      <Form<IUser> elements={elements} onSubmit={handleSignIn} />
+    </>
+  );
 };
 export default LoginScreen;
-
