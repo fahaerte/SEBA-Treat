013d12d33c20503e69375faf34cb0db87bb59a1c
johannes <johannes.wiest@muenchen-mail.de>
2022-06-27 22:24:56 +0200

    refactor update mealReservationState

--- packages/backend/src/utils/exceptions/invalidMealReservationState.exception.ts
+++ packages/backend/src/utils/exceptions/invalidMealReservationState.exception.ts
@@ -1,9 +1,9 @@
 import HttpException from "./http.exception";
 
 class InvalidMealReservationStateException extends HttpException {
-    constructor(message: string) {
-        super(409, message);
-    }
+  constructor(message: string) {
+    super(409, message);
+  }
 }
 
 export default InvalidMealReservationStateException;
