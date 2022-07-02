019be8f3aac4a6e2eaad45d9c161e78166f20a0f
Fabian Härtel <fabian.haertel@tum.de>
2022-07-02 10:31:40 +0200

    Added Signout-Button to AuthStatus

--- packages/webapp/src/utils/AuthStatus.tsx
+++ packages/webapp/src/utils/AuthStatus.tsx
@@ -1,22 +1,24 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../utils/AuthProvider";
+import React, {useContext} from "react";
+import {AuthContext} from "../utils/AuthProvider";
+import Button from "../components/Button/Button";
 
 const AuthStatus = () => {
-  const authContext = useContext(AuthContext);
+    const authContext = useContext(AuthContext);
 
-  const signout = () => {
-    authContext.setToken(undefined);
-    console.log("logged out!");
-  };
+    const signout = () => {
+        authContext.setToken(undefined);
+        console.log("logged out!");
+    };
 
-  return (
-    <>
-      {authContext.token ? (
-        <p>Logged in!</p>
-      ) : (
-        <p>You are not logged in.</p>
-      )}
-    </>
-  );
+    return (
+        <>
+            {authContext.token ?
+                <div>
+                    <p>Logged in!</p>
+                    <Button onClick={signout}>Sign Out</Button>
+                </div>
+                : <p>You are not logged in.</p>}
+        </>
+    );
 };
 export default AuthStatus;
