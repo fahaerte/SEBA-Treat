57953ab2c0a4f805d8d0a9a3699adcef13ba8ea6
Fabian Haertel <fabian.haertel@tum.de>
2022-07-09 13:12:14 +0200

    installation of axios

---
+++ packages/webapp/src/api/authApi.ts
@@ -0,0 +1,10 @@
+import { baseApi } from "./baseApi";
+import { IUser, IUserCredentials } from "@treat/lib-common";
+
+export const login = async (credentials: IUserCredentials) => {
+  return await baseApi.post("/users/login", credentials);
+}
+
+export const register = async (user: IUser) => {
+  return await baseApi.post("/users/register", user);
+}
---
+++ packages/webapp/src/api/baseApi.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { ConfigService } from "../utils/ConfigService";
+
+export const baseApi = axios.create({
+  baseURL: `${new ConfigService().get("API_URL")}`,
+})
