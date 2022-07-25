fe97ce123f4061021931397616a4c7012c888703
simon-roe <s2401r@gmail.com>
2022-07-25 15:27:59 +0200

    disabled reservation button on MealOfferDetailScreen.tsx when e.g. offer has expired

--- packages/webapp/src/components/MealRequestCard/MealRequestCard.tsx
+++ packages/webapp/src/components/MealRequestCard/MealRequestCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { ICard } from "../ui/Card/ICard";
 // import { SCMealRequestCard } from "./styles";
 import { Button, Card, CardBody, Col, Icon, Row, Typography } from "../";
+import { getCookie } from "../../utils/auth/CookieProvider";
 
 // TODO: replace inline styles with component styles
 
@@ -10,91 +11,105 @@ const MealRequestCard = ({
   productName,
   mealPrice,
   transactionFee,
+  disableButton,
+  disabledText = "",
   buttonAction,
 }: {
   className?: string;
   productName: string;
   mealPrice: number;
   transactionFee: number;
+  disableButton: boolean;
+  disabledText?: string;
   buttonAction: () => void;
-}) => (
-  <Card className={`${className} mb-3`}>
-    <CardBody className={"my-3"}>
-      <Typography variant={"h1"} className={"mb-3"}>
-        Order {productName}
-      </Typography>
-      <div
-        style={{
-          padding: ".5rem .7rem",
-          marginBottom: "1rem",
-          backgroundColor: "#EFEFEF",
-          border: "1px solid #CFCFCF",
-          borderRadius: "3px",
-          color: "#454545",
-        }}
-      >
+}) => {
+  return (
+    <Card className={`${className} mb-3`}>
+      <CardBody className={"my-3"}>
+        <Typography variant={"h1"} className={"mb-3"}>
+          Order {productName}
+        </Typography>
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
+            padding: ".5rem .7rem",
             marginBottom: "1rem",
+            backgroundColor: "#EFEFEF",
+            border: "1px solid #CFCFCF",
+            borderRadius: "3px",
+            color: "#454545",
           }}
         >
-          <span style={{ fontWeight: "bold", width: "auto" }}>
-            How to buy a meal
-          </span>
-          <Icon type={"infoCircle"} size={"md"} />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: "1rem",
+            }}
+          >
+            <span style={{ fontWeight: "bold", width: "auto" }}>
+              How to buy a meal
+            </span>
+            <Icon type={"infoCircle"} size={"md"} />
+          </div>
+          <ol style={{ paddingLeft: "1rem" }}>
+            <li style={{ paddingLeft: ".5rem" }}>
+              Check allergens if you have any
+            </li>
+            <li style={{ paddingLeft: ".5rem" }}>
+              Make a request for the meal
+            </li>
+            <li style={{ paddingLeft: ".5rem" }}>
+              Wait until the seller accepts the request
+            </li>
+            <li style={{ paddingLeft: ".5rem" }}>
+              Confirm the transaction (<u>only then, you are charged</u>)
+            </li>
+          </ol>
         </div>
-        <ol style={{ paddingLeft: "1rem" }}>
-          <li style={{ paddingLeft: ".5rem" }}>
-            Check allergens if you have any
-          </li>
-          <li style={{ paddingLeft: ".5rem" }}>Make a request for the meal</li>
-          <li style={{ paddingLeft: ".5rem" }}>
-            Wait until the seller accepts the request
-          </li>
-          <li style={{ paddingLeft: ".5rem" }}>
-            Confirm the transaction (<u>only then, you are charged</u>)
-          </li>
-        </ol>
-      </div>
-      <Row>
-        <Col>
-          <Typography>Meal Price</Typography>
-        </Col>
-        <Col>
-          <Typography align={"end"}>{mealPrice} Cr</Typography>
-        </Col>
-      </Row>
-      <Row justify={"between"}>
-        <Col>
-          <Typography>
-            Service Fee ({Math.round((transactionFee / mealPrice) * 100)}%)
-          </Typography>
-        </Col>
-        <Col>
-          <Typography align={"end"}>{transactionFee} Cr</Typography>
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
-          <Typography variant={"h3"}>Total</Typography>
-        </Col>
-        <Col>
-          <Typography variant={"h3"} align={"end"}>
-            {mealPrice + transactionFee} Cr
-          </Typography>
-        </Col>
-      </Row>
-      <Row style={{ margin: "1rem 0 0 0" }}>
-        <Button className="px-3" onClick={buttonAction}>
-          Request meal
-        </Button>
-      </Row>
-    </CardBody>
-  </Card>
-);
+        <Row>
+          <Col>
+            <Typography>Meal Price</Typography>
+          </Col>
+          <Col>
+            <Typography align={"end"}>{mealPrice} Cr</Typography>
+          </Col>
+        </Row>
+        <Row justify={"between"}>
+          <Col>
+            <Typography>
+              Service Fee ({Math.round((transactionFee / mealPrice) * 100)}%)
+            </Typography>
+          </Col>
+          <Col>
+            <Typography align={"end"}>{transactionFee} Cr</Typography>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <Typography variant={"h3"}>Total</Typography>
+          </Col>
+          <Col>
+            <Typography variant={"h3"} align={"end"}>
+              {mealPrice + transactionFee} Cr
+            </Typography>
+          </Col>
+        </Row>
+        <Row style={{ margin: "1rem 0 0 0" }}>
+          {!disableButton ? (
+            <Button className="px-3" onClick={buttonAction}>
+              Reserve meal
+            </Button>
+          ) : (
+            <Typography align={"center"} color={"warning"}>
+              {disabledText}
+            </Typography>
+          )}
+        </Row>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default MealRequestCard;
--- packages/webapp/src/screens/Meal/MealOfferDetailScreen.tsx
+++ packages/webapp/src/screens/Meal/MealOfferDetailScreen.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import {
+  Button,
   Col,
   dangerToast,
   MealDetails,
@@ -16,11 +17,17 @@ import { getMealOffer, requestMealOffer } from "../../api/mealApi";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import MealRequestCard from "../../components/MealRequestCard/MealRequestCard";
 import { AxiosError } from "axios";
+import { getCookie } from "../../utils/auth/CookieProvider";
 
 export const MealOfferDetailScreen = () => {
+  const userId = getCookie("userId");
+
   const { mealOfferId } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [enableReservation, setEnableReservation] = useState(true);
+  const [reservationText, setReservationText] = useState("Reserve meal");
 
   const { data: mealOffer, isLoading: mealOfferIsLoading } = useQuery(
     "getMealOffer",
@@ -28,9 +35,47 @@ export const MealOfferDetailScreen = () => {
     {
       onSuccess: (response) => {
         console.log(response);
+
+        const nowDate = new Date();
+        const startDate = new Date(response.startDate);
+        const endDate = new Date(response.endDate);
+
+        if (startDate > nowDate) {
+          setReservationText("The offer has not started yet.");
+          setEnableReservation(false);
+        }
+        if (endDate < nowDate) {
+          setReservationText("The offer has expired.");
+          setEnableReservation(false);
+        }
+        if (userId === response.user._id) {
+          setReservationText("You cannot reserve your own meal.");
+          setEnableReservation(false);
+        }
       },
     }
   );
+
+  // useEffect(() => {
+  //   console.log(mealOffer);
+  //   const nowDate = new Date();
+  //   const endDate = new Date(mealOffer.endDate);
+  //   console.log("nowDate: ", nowDate);
+  //   console.log("still valid: ", endDate > nowDate);
+  //   if (userId !== mealOffer.user._id) {
+  //     setEnableReservation(true);
+  //   }
+  // }, [mealOffer]);
+
+  function handleEditClick() {
+    if (userId && userId === mealOffer.user._id) {
+      console.log("Edit click");
+    } else {
+      dangerToast({
+        message: "You can only edit your own meals.",
+      });
+    }
+  }
 
   const requestMealMutation = useMutation(requestMealOffer, {
     onSuccess: () => {
@@ -50,7 +95,9 @@ export const MealOfferDetailScreen = () => {
           message: "Unexpected server error. The meal could not be reserved.",
         });
       }
-      navigate("/login", { state: { from: location } });
+      if (!userId) {
+        navigate("/login", { state: { from: location } });
+      }
     },
   });
 
@@ -73,20 +120,23 @@ export const MealOfferDetailScreen = () => {
           </Container>
         ) : mealOffer ? (
           <Container className={""}>
-            <div
-              className={"pt-5"}
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <PageHeading style={{ marginRight: "0.5em" }}>
-                <u>{mealOffer.title}</u>
-              </PageHeading>
-              {mealOffer.categories.map((category: string) => (
-                <Tag key={category}>{category}</Tag>
-              ))}
-            </div>
+            <Row className={"pt-5 mr-1"}>
+              <Col className={"d-flex align-items-center"}>
+                <PageHeading className={"text-nowrap"}>
+                  <u>{mealOffer.title}</u>
+                </PageHeading>
+                {mealOffer.categories.map((category: string) => (
+                  <Tag key={category}>{category}</Tag>
+                ))}
+              </Col>
+              <Col className={"d-flex align-items-center justify-content-end"}>
+                {userId && userId === mealOffer.user._id && (
+                  <Button className="px-3" onClick={handleEditClick}>
+                    Edit
+                  </Button>
+                )}
+              </Col>
+            </Row>
             <UserPreview
               img={"undefined"}
               firstName={mealOffer.user.firstName}
@@ -118,7 +168,10 @@ export const MealOfferDetailScreen = () => {
                 <SectionHeading>Description</SectionHeading>
                 <p>{mealOffer.description}</p>
                 <SectionHeading>Location</SectionHeading>
-                <p>{mealOffer.transactionFee}</p>
+                <p>
+                  {mealOffer.transactionFee} km (You will see the exact location
+                  once the seller accepted your reservation.)
+                </p>
                 <SectionHeading>List of Allergens</SectionHeading>
                 {mealOffer.allergens.map((allergen: string) => (
                   <Tag key={allergen}>{allergen}</Tag>
@@ -126,10 +179,11 @@ export const MealOfferDetailScreen = () => {
               </Col>
               <Col style={{ maxWidth: "400px" }}>
                 <MealRequestCard
-                  key={mealOffer._id}
                   productName={mealOffer.title}
                   mealPrice={mealOffer.price}
                   transactionFee={mealOffer.transactionFee}
+                  disableButton={!enableReservation}
+                  disabledText={reservationText}
                   buttonAction={() => handleRequestClick()}
                 />
               </Col>
