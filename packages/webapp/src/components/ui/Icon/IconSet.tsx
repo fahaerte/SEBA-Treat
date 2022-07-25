fdac74e2176e49137bbea7abb508d0f62a6a7199
Luoshan Zheng <ge47kod@mytum.de>
2022-07-25 18:14:40 +0200

    lrz: fix people-fill Icon

--- packages/webapp/src/components/ui/Icon/IconSet.tsx
+++ packages/webapp/src/components/ui/Icon/IconSet.tsx
@@ -159,16 +159,12 @@ export const IconSet = () => (
       </symbol>
       <symbol id="people-fill" viewBox="0 0 16 16">
         <title>people-fill</title>
-        <path
-          fillRule="evenodd"
-          d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-        />
         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         <path
           fillRule="evenodd"
           d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
         />
-        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />{" "}
+        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
       </symbol>
     </defs>
   </SCIconSet>
