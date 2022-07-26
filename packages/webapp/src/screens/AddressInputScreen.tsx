c885d2cb51e9af67c3899cd520a1ec8c0cb2ff55
Fabian Haertel <fabian.haertel@tum.de>
2022-07-26 22:00:55 +0200

    first draft

--- packages/webapp/src/screens/AddressInputScreen.tsx
+++ packages/webapp/src/screens/AddressInputScreen.tsx
@@ -4,9 +4,12 @@ import { IStringObject } from "@treat/lib-common";
 import { useLocation, useNavigate } from "react-router-dom";
 import { addressElement } from "../components/AddressInput/AddressInput";
 import { setCookie } from "../utils/auth/CookieProvider";
-import { Container, Icon, Typography } from "../components";
+import { CardImage, Col, Container, Icon, Typography } from "../components";
 import { AddressInputLayout } from "../components/AddressInput/AdressInputLayout";
 import { Header } from "../components/ui/Header/Header";
+import AddressPictogramm from "../assets/AddressPictogramm.png";
+import MealPictogramm from "../assets/MealPictogramm.png";
+import MoneyPictogramm from "../assets/MoneyPictogramm.png";
 
 export const AddressInputScreen = () => {
   const navigate = useNavigate();
@@ -26,6 +29,8 @@ export const AddressInputScreen = () => {
     navigate(from, { replace: true });
   };
 
+  // TODO:  Address-Input schmaler machen
+  // TODO: Insert Pictogramms for 2nd row
   return (
     <>
       <Header />
@@ -52,6 +57,100 @@ export const AddressInputScreen = () => {
                 ),
               }}
             />
+          </Container>
+        </div>
+        <div>
+          <hr />
+          <Container>
+            <Typography variant={"h1"} color={"secondary"}>
+              How Treat works
+            </Typography>
+            <br />
+            <Typography variant={"h2"} color={"secondary"}>
+              Get food offered in your neighborhood!
+            </Typography>
+            <div className={"d-flex justify-content-around"}>
+              <Col>
+                <img
+                  src={AddressPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Set your location
+                </Typography>
+              </Col>
+              <Col>
+                <img
+                  src={MealPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Choose a meal
+                </Typography>
+              </Col>
+              <Col>
+                <img
+                  src={MoneyPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Pay with virtual credits
+                </Typography>
+              </Col>
+            </div>
+          </Container>
+          <br />
+          <Container>
+            <Typography variant={"h2"} color={"secondary"}>
+              Offer your leftovers to your neighborhood!
+            </Typography>
+            <div className={"d-flex justify-content-around"}>
+              <Col>
+                <img
+                  src={AddressPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Insert leftovers
+                </Typography>
+              </Col>
+              <Col>
+                <img
+                  src={MealPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Get credits in return
+                </Typography>
+              </Col>
+              <Col>
+                <img
+                  src={MoneyPictogramm}
+                  style={{
+                    maxWidth: "70%",
+                    maxHeight: "70%",
+                  }}
+                />
+                <Typography variant={"h3"} color={"secondary"}>
+                  Reduce food waste and safe resources
+                </Typography>
+              </Col>
+            </div>
           </Container>
         </div>
       </AddressInputLayout>
