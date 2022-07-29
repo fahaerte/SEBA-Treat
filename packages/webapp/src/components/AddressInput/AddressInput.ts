86f5d2a7d3e963d9219a4b87ed854d43f088a137
Fabian Haertel <fabian.haertel@tum.de>
2022-07-29 12:55:16 +0200

    improved address-input

--- packages/webapp/src/components/AddressInput/AddressInput.ts
+++ packages/webapp/src/components/AddressInput/AddressInput.ts
@@ -2,7 +2,14 @@ import { FormHelper, IFormRow } from "../ui";
 import { IStringObject } from "@treat/lib-common";
 import { getCookie } from "../../utils/auth/CookieProvider";
 
-const address = getCookie("address");
+const address = () => {
+  const cookieValue = getCookie("address");
+  if (cookieValue) {
+    return cookieValue;
+  } else {
+    return "Arcisstrasse 11, 80335 Munich";
+  }
+};
 
 export const addressElement: IFormRow<IStringObject>[] = [
   FormHelper.createInput({
